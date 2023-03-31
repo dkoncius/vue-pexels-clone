657ea572eef4929c07ce86e36793f18c4b18dfc1
dkoncius <as.koncius@gmail.com>
2023-03-31 14:34:22 +0300

    /vue-pexels-clone/

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/',
+  base: '/vue-pexels-clone/',
   server: {
     open: true
   }
